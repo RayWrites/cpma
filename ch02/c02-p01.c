89aa070239a11815eaad26410f31a06a01186ecb
ray <github@raywrites.com>
2019-01-15 21:17:05 -0800

    Adds Chapter 2, Programming Project 1

---
+++ ch02/c02-p01.c
@@ -0,0 +1,17 @@
+// C Programming: A Modern Approach, Second Edition
+// Chapter 2, Programming Project 1
+// Ray Santos -- 15Jan2019 
+
+#include <stdio.h>
+
+int main(void)
+{
+   printf("       *\n");
+   printf("      *\n");
+   printf("     *\n");
+   printf("*   *\n");
+   printf(" * *\n");
+   printf("  *\n");
+
+  return 0;
+}
